aca67e861ced609ca4650d4c4a4d1a0145bf2cd5
NaheedRayan <naheed27ray@gmail.com>
2025-02-24 00:04:19 +0600

    added node mailer

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+        connection.on("error", (error) => {
+            console.log("Error connecting to database");
+            console.log(error);
+        });
+
+        connection.once("error", () => {
+            console.log("Connected to database");
+            process.exit(0);
+        });
+
+    } catch (error) {
+        console.log("Error connecting to database");
+        console.log(error);
+    }
+}
